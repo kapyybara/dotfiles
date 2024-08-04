4c0bb8673873a98da1793f9cdb16a0a5d248f15d
Pham Tien <iwkuthedev@gmail.com>
2024-08-04 11:35:54 +0700

    add vim config from lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymaps = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymaps.set("n", "+", "<C-a>")
+keymaps.set("n", "+", "<C-x>")
+
+-- Jumplist
+keymaps.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymaps.set("n", "te", "tabedit", opts)
+keymaps.set("n", "<tab>", ":tabnext<Return>", opts)
+keymaps.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymaps.set("n", "ss", ":split<Return>", opts)
+keymaps.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymaps.set("n", "sh", "<C-w>h")
+keymaps.set("n", "sj", "<C-w>j")
+keymaps.set("n", "sk", "<C-w>k")
+keymaps.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymaps.set("n", "<C-w><left>", "<C-w><")
+keymaps.set("n", "<C-w><right>", "<C-w>>")
+keymaps.set("n", "<C-w><up>", "<C-w>+")
+keymaps.set("n", "<C-w><down>", "<C-w>-")
