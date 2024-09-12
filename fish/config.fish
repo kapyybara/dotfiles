e1b4c7971eba82ceb7bb1d8738cd82c10a09a8b6
Pham Tien <iwkuthedev@gmail.com>
2024-09-12 13:48:02 +0700

    add fish

--- fish/config.fish
+++ fish/config.fish
@@ -75,3 +75,17 @@ alias nf neofetch
 
 
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/samsa/anaconda3/bin/conda
+    eval /home/samsa/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/samsa/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/home/samsa/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/samsa/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
