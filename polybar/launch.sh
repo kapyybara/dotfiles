7b52bc7c841c69e80a035593ef05c3059c7f6315
pham tien <iwkuthedev@gmail.com>
2025-01-12 15:01:59 +0700

    update fish

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,9 +2,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $USER -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar --reload mybar &
+polybar --reload mybar  --config=./config.ini &
 
 echo "Polybar launched..."
